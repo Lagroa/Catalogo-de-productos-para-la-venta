44da44ad63c522d462d4616c2748ea813cb9eebb
Laura andrea gomez roa <lagroa@gmail.com>
2024-11-06 20:13:39 -0500

    Add files via upload

---
+++ credicel_Catalogo de Ventas GAVASSA_Perfiles.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE  IF NOT EXISTS `credicel_Catalogo de Ventas GAVASSA` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `credicel_Catalogo de Ventas GAVASSA`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 190.90.160.172    Database: credicel_Catalogo de Ventas GAVASSA
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.19-MariaDB-cll-lve
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Perfiles`
+--
+
+DROP TABLE IF EXISTS `Perfiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Perfiles` (
+  `idPerfiles_Clientes` int(11) NOT NULL,
+  `Descripcion_Perfil` varchar(45) NOT NULL,
+  PRIMARY KEY (`idPerfiles_Clientes`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci COMMENT='Perifles de asignación de precios a los clientes';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Perfiles`
+--
+
+LOCK TABLES `Perfiles` WRITE;
+/*!40000 ALTER TABLE `Perfiles` DISABLE KEYS */;
+INSERT INTO `Perfiles` VALUES (1,'Distribuidor'),(2,'Mayorista'),(3,'Abarrotero'),(4,'Autoservicio'),(5,'Cadenas regionales'),(6,'Cadenas nacionales'),(7,'Tiendas');
+/*!40000 ALTER TABLE `Perfiles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-06 20:01:03
---
+++ credicel_Catalogo de Ventas GAVASSA_Proveedores.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE  IF NOT EXISTS `credicel_Catalogo de Ventas GAVASSA` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `credicel_Catalogo de Ventas GAVASSA`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 190.90.160.172    Database: credicel_Catalogo de Ventas GAVASSA
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.19-MariaDB-cll-lve
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Proveedores`
+--
+
+DROP TABLE IF EXISTS `Proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Proveedores` (
+  `idProveedores` int(11) NOT NULL,
+  `Codigo proveedor` varchar(45) NOT NULL,
+  `Proveedor` varchar(45) NOT NULL,
+  PRIMARY KEY (`idProveedores`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Proveedores`
+--
+
+LOCK TABLES `Proveedores` WRITE;
+/*!40000 ALTER TABLE `Proveedores` DISABLE KEYS */;
+INSERT INTO `Proveedores` VALUES (1,'P890200408','GAVASSA'),(2,'P890905407','FINCA'),(3,'P901650130','ARROZ GELVEZ'),(4,'P901815618','AGROINVERSA'),(5,'P900087414','RIOPAILA');
+/*!40000 ALTER TABLE `Proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-06 20:01:06
